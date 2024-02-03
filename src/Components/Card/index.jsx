e78c3a90487b23f5763c67ae4931031c59f5a304
Jeison Guerrero <jei_son1516@hotmail.com>
2024-02-03 08:27:58 -0500

    Refactor Card component to accept data props

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,22 +1,24 @@
-const Card = () => {
+const Card = (data) => {
   return (
     <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
       <figure className="relative mb-2 w-full h-4/5">
-        <span className="absolute bottom-2 left-2 bg-white/60 rounded-lg text-black text-sm px-3 py-0.5">
-          Electronics
+        <span className="absolute bottom-2 left-2 bg-neutral-300/60 rounded-lg text-black text-base font-semibold px-3 py-0.5">
+          {data.data.category}
         </span>
         <img
           className="w-full h-full rounded-lg object-cover"
-          src="https://images.pexels.com/photos/815494/pexels-photo-815494.jpeg"
-          alt="Headphones"
+          src={data.data.image}
+          alt={data.data.title}
         />
         <div className="absolute top-1 right-1 flex justify-center items-center bg-white w-6 h-6 rounded-full p-1">
           +
         </div>
       </figure>
-      <p className="flex justify-between">
-        <span className="text-sm font-light">Headphones</span>
-        <span className="text-lg font-medium">$300</span>
+      <p className="flex justify-between items-center">
+        <span className="text-sm font-normal w-36 h-5 truncate ...">
+          {data.data.title}
+        </span>
+        <span className="text-lg font-medium">{data.data.price}</span>
       </p>
     </div>
   );

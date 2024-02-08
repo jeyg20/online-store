077f7318834b09ee7f3d5d4f44f24f370e6f3163
Jeison Guerrero <jei_son1516@hotmail.com>
2024-02-08 14:46:28 -0500

    Refactor Home component to handle loading and error states

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,22 +1,30 @@
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function Home() {
-  const [Products, setProducts] = useState(null);
+  const [products, setProducts] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       try {
         const response = await fetch(
-          "https://fakestoreapi.com/products?limit=100"
+          "https://fakestoreapi.com/products?limit=0"
         );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
         const data = await response.json();
         setProducts(data);
         console.log(data);
       } catch (error) {
-        console.error(error);
+        setError(error.message);
+        console.error("Error fetching date: ", error);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchData();
@@ -24,11 +32,19 @@ function Home() {
 
   return (
     <Layout>
-      Home
-      <section className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
-        {Products?.map((product) => (
-          <Card key={product.id} data={product} />
-        ))}
+      <h1 className="text-xl font-semibold">Home</h1>
+      <section className="grid gap-4 grid-cols-4 w-full max-w-screen-lg justify-center items-center pt-10">
+        {isLoading ? (
+          <p className="text-center col-span-4 row-span-2">Loading...</p>
+        ) : error ? (
+          <p className="text-center col-span-4 row-span-2">
+            Error fetching data
+          </p>
+        ) : products?.length ? (
+          products?.map((product) => <Card key={product.id} data={product} />)
+        ) : (
+          <p className="text-center col-span-4 row-span-2">No products</p>
+        )}
       </section>
     </Layout>
   );
